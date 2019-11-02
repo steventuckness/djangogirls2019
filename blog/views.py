4c4971d34178389c01dda8c76362dbb811edbe64
steven tuckness <stuckne1@gmail.com>
2019-11-02 09:01:20 -0500

    display db data on post list page

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    print(timezone.now());
+    
+    posts = Post.objects.order_by('published_date'); # just get them all now inca
+    #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
